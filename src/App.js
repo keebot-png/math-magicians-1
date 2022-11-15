e9449fea591541c261d9ea3d00557b13f32caaad
keebot-png <kee.bot@icloud.com>
2022-11-16 00:21:55 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import Quotes from './components/quotePage/quote';
 import HomePage from './components/homePage/home';
 import Navbar from './components/navbarHeading/navbar';
 
-/* eslint-disable */
 class App extends React.PureComponent {
   render() {
     return (
       <div>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quote" element={<Quotes />} />
-    </Routes>
-  </div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quotes />} />
+        </Routes>
+      </div>
     );
   }
 }
